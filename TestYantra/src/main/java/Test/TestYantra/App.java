21ffc71430d2b83ef8fd04bae8858b1ff20bec68
USER <USER@DESKTOP-9V0BM5U>
2021-12-11 11:40:51 +0530

    will keep you updated

---
+++ TestYantra/src/main/java/Test/TestYantra/App.java
@@ -0,0 +1,13 @@
+package Test.TestYantra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
